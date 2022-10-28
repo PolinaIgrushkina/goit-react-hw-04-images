3da12a83035b71b8be9c6e46b9c6ba5a706b3340
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-10-28 18:17:09 +0300

    Fixed App, Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,19 @@
-import { Component } from "react";
+import { useState } from "react";
 import Searchbar from "./Searchbar/Searchbar";
 import ImageGallery from './ImageGallery/ImageGallery';
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    searchPicture: '',
+export function App() {
+  const[searchPicture, setSearchPicture] = useState('');
+
+  const handleSubmit = searchWord => {
+    setSearchPicture(searchWord);
   }
 
-  handleSubmit = searchWord => {
-    this.setState({searchPicture: searchWord})
-  }
-
-  render() {
     return (
       <div className={css.app}>
-        <Searchbar onSubmit={this.handleSubmit} />
-        <ImageGallery searchPicture={this.state.searchPicture} />
+        <Searchbar onSubmit={handleSubmit} />
+        <ImageGallery searchPicture={searchPicture} />
       </div>
     );
-  }
 };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,33 +1,30 @@
 import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
 import { GrFormSearch } from "react-icons/gr";
-import { Component } from 'react';
+import { useState } from 'react';
 
-export default class Searchbar extends Component {
-state = {
-  searchWord: ''
-}
-  
-handleChange = event => {
-  this.setState({ searchWord: event.currentTarget.value.toLowerCase() });
-  };
+export default function Searchbar({onSubmit}) {
 
-handleSubmit = event => {
+const [searchWord, setSearchWord] = useState('');
+
+const handleChange = event => {
+  setSearchWord(event.currentTarget.value.toLowerCase());
+};
+
+const handleSubmit = event => {
   event.preventDefault();
 
-  if (this.state.searchWord.trim() === '') {
+  if (searchWord.trim() === '') {
     alert('Пожалуста введите поисковый запрос.');
     return;
   }
 
-  this.props.onSubmit(this.state.searchWord);
-  this.setState({ searchWord: '' });
-  };  
+  onSubmit(searchWord);
+  setSearchWord('');
+};  
   
-  
-render() {
   return (<header className={css.searchbar}>
-    <form className={css.form} onSubmit={this.handleSubmit}>
+    <form className={css.form} onSubmit={handleSubmit}>
       <button type="submit" className={css.button}>
         <GrFormSearch className={css.button_label} />
       </button>
@@ -38,13 +35,12 @@ render() {
         autoComplete="off"
         autoFocus
         placeholder="Search images and photos"
-        value={this.state.searchWord}
-        onChange={this.handleChange}
+        value={searchWord}
+        onChange={handleChange}
       />
     </form>
   </header>)
 };
-}
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
